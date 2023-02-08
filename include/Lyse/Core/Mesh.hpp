23468e344cce04939a5491c2c2e6c5bd69fe19f9
Reiex <reiex.marius38@gmail.com>
2023-02-08 10:04:34 +0100

    Progress in Mesh API (better creation, update, getters...)

--- include/Lyse/Core/Mesh.hpp
+++ include/Lyse/Core/Mesh.hpp
@@ -16,7 +16,9 @@ namespace lys
 	{
 		public:
 
-			Mesh();
+			template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage> Mesh(const std::initializer_list<TVertex>& vertices, TVertexStorage vertexStorage, TIndexStorage indexStorage);
+			template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage> Mesh(const TVertex* vertices, uint32_t vertexCount, TVertexStorage vertexStorage, TIndexStorage indexStorage);
+			template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage> Mesh(const std::initializer_list<TVertex>& vertices, TVertexStorage vertexStorage, const std::initializer_list<uint32_t>& indices, TIndexStorage indexStorage);
 			template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage> Mesh(const TVertex* vertices, uint32_t vertexCount, TVertexStorage vertexStorage, const uint32_t* indices, uint32_t indexCount, TIndexStorage indexStorage);
 			template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage> Mesh(const std::filesystem::path& path, TVertexStorage vertexStorage, TIndexStorage indexStorage);
 			Mesh(const Mesh<TVertex>& mesh);
@@ -25,11 +27,13 @@ namespace lys
 			Mesh<TVertex>& operator=(const Mesh<TVertex>& mesh);
 			Mesh<TVertex>& operator=(Mesh<TVertex>&& mesh);
 
+			
+			template<CBufferStorageSpecifier TStorage> void createNewVertices(const TVertex* vertices, uint32_t count, TStorage storage);
+			template<CBufferStorageSpecifier TStorage> void createNewIndices(const uint32_t* indices, uint32_t count, TStorage storage);
+			template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage> void createFromFile(const std::filesystem::path& path, TVertexStorage vertexStorage, TIndexStorage indexStorage);
 
-			template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage> void createNew(const TVertex* vertices, uint32_t vertexCount, TVertexStorage vertexStorage, const uint32_t* indices, uint32_t indexCount, TIndexStorage indexStorage);
-			template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage> void createNew(const std::filesystem::path& path, TVertexStorage vertexStorage, TIndexStorage indexStorage);
-
-			// TODO: Update
+			void updateVertices(const TVertex* vertices, uint32_t count, uint32_t offset = 0);
+			void updateIndices(const uint32_t* indices, uint32_t count, uint32_t offset = 0);
 
 			void destroy();
 
@@ -37,13 +41,20 @@ namespace lys
 			virtual void draw() const override final;
 
 
-			// TODO: getters VAO, EBO, VBO...
+			const spl::VertexArray& getVertexArray() const;
+			spl::VertexArray& getVertexArray();
+			const spl::Buffer& getVertexBuffer() const;
+			spl::Buffer& getVertexBuffer();
+			const spl::Buffer& getElementBuffer() const;
+			spl::Buffer& getElementBuffer();
 			bool isValid() const;
 
 
 			virtual ~Mesh() override final = default;
 
 		private:
+
+			Mesh();
 
 			virtual void draw(const spl::ShaderProgram& program, const scp::f32mat4x4& transform) const override final;
 
--- include/Lyse/Core/templates/Mesh.hpp
+++ include/Lyse/Core/templates/Mesh.hpp
@@ -29,16 +29,43 @@ namespace lys
 
 	template<CVertex TVertex>
 	template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage>
+	Mesh<TVertex>::Mesh(const std::initializer_list<TVertex>& vertices, TVertexStorage vertexStorage, TIndexStorage indexStorage) : Mesh(vertices.begin(), vertices.size(), vertexStorage, indexStorage)
+	{
+	}
+
+	template<CVertex TVertex>
+	template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage>
+	Mesh<TVertex>::Mesh(const TVertex* vertices, uint32_t vertexCount, TVertexStorage vertexStorage, TIndexStorage indexStorage) : Mesh()
+	{
+		std::vector<uint32_t> indices(vertexCount);
+		for (uint32_t i = 0; i < vertexCount; ++i)
+		{
+			indices[i] = i;
+		}
+
+		createNewVertices(vertices, vertexCount, vertexStorage);
+		createNewIndices(indices.data(), indices.size(), indexStorage);
+	}
+
+	template<CVertex TVertex>
+	template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage>
+	Mesh<TVertex>::Mesh(const std::initializer_list<TVertex>& vertices, TVertexStorage vertexStorage, const std::initializer_list<uint32_t>& indices, TIndexStorage indexStorage) : Mesh(vertices.begin(), vertices.size(), vertexStorage, indices.begin(), indices.size(), indexStorage)
+	{
+	}
+
+	template<CVertex TVertex>
+	template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage>
 	Mesh<TVertex>::Mesh(const TVertex* vertices, uint32_t vertexCount, TVertexStorage vertexStorage, const uint32_t* indices, uint32_t indexCount, TIndexStorage indexStorage) : Mesh()
 	{
-		createNew(vertices, vertexCount, vertexStorage, indices, indexCount, indexStorage);
+		createNewVertices(vertices, vertexCount, vertexStorage);
+		createNewIndices(indices, indexCount, indexStorage);
 	}
 	
 	template<CVertex TVertex>
 	template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage>
 	Mesh<TVertex>::Mesh(const std::filesystem::path& path, TVertexStorage vertexStorage, TIndexStorage indexStorage) : Mesh()
 	{
-		createNew(path, vertexStorage, indexStorage);
+		createFromFile(path, vertexStorage, indexStorage);
 	}
 
 	template<CVertex TVertex>
@@ -77,20 +104,26 @@ namespace lys
 		return *this;
 	}
 
-	template<CVertex TVertex>
-	template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage>
-	void Mesh<TVertex>::createNew(const TVertex* vertices, uint32_t vertexCount, TVertexStorage vertexStorage, const uint32_t* indices, uint32_t indexCount, TIndexStorage indexStorage)
-	{
-		_vbo.createNew(sizeof(TVertex) * vertexCount, vertexStorage, vertices);
-		_ebo.createNew(sizeof(uint32_t) * indexCount, indexStorage, indices);
 
+	template<CVertex TVertex>
+	template<CBufferStorageSpecifier TStorage>
+	void Mesh<TVertex>::createNewVertices(const TVertex* vertices, uint32_t count, TStorage storage)
+	{
+		_vbo.createNew(sizeof(TVertex) * count, storage, vertices);
 		_vao.bindArrayBuffer(_vbo, 0, 0, sizeof(TVertex));
+	}
+
+	template<CVertex TVertex>
+	template<CBufferStorageSpecifier TStorage>
+	void Mesh<TVertex>::createNewIndices(const uint32_t* indices, uint32_t count, TStorage storage)
+	{
+		_ebo.createNew(sizeof(uint32_t) * count, storage, indices);
 		_vao.bindElementBuffer(_ebo);
 	}
 
 	template<CVertex TVertex>
 	template<CBufferStorageSpecifier TVertexStorage, CBufferStorageSpecifier TIndexStorage>
-	void Mesh<TVertex>::createNew(const std::filesystem::path& path, TVertexStorage vertexStorage, TIndexStorage indexStorage)
+	void Mesh<TVertex>::createFromFile(const std::filesystem::path& path, TVertexStorage vertexStorage, TIndexStorage indexStorage)
 	{
 		std::vector<TVertex> vertices;
 		std::vector<uint32_t> indices;
@@ -99,8 +132,21 @@ namespace lys
 
 		if (!vertices.empty() && !indices.empty())
 		{
-			createNew(vertices.data(), vertices.size(), vertexStorage, indices.data(), indices.size(), indexStorage);
+			createNewVertices(vertices.data(), vertices.size(), vertexStorage);
+			createNewIndices(indices.data(), indices.size(), indexStorage);
 		}
+	}
+
+	template<CVertex TVertex>
+	void Mesh<TVertex>::updateVertices(const TVertex* vertices, uint32_t count, uint32_t offset)
+	{
+		_vbo.update(vertices, sizeof(TVertex) * count, sizeof(TVertex) * offset);
+	}
+
+	template<CVertex TVertex>
+	void Mesh<TVertex>::updateIndices(const uint32_t* indices, uint32_t count, uint32_t offset)
+	{
+		_ebo.update(indices, sizeof(uint32_t) * count, sizeof(uint32_t) * offset);
 	}
 
 	template<CVertex TVertex>
@@ -119,6 +165,42 @@ namespace lys
 	}
 
 	template<CVertex TVertex>
+	const spl::VertexArray& Mesh<TVertex>::getVertexArray() const
+	{
+		return _vao;
+	}
+
+	template<CVertex TVertex>
+	spl::VertexArray& Mesh<TVertex>::getVertexArray()
+	{
+		return _vao;
+	}
+
+	template<CVertex TVertex>
+	const spl::Buffer& Mesh<TVertex>::getVertexBuffer() const
+	{
+		return _vbo;
+	}
+
+	template<CVertex TVertex>
+	spl::Buffer& Mesh<TVertex>::getVertexBuffer()
+	{
+		return _vbo;
+	}
+
+	template<CVertex TVertex>
+	const spl::Buffer& Mesh<TVertex>::getElementBuffer() const
+	{
+		return _ebo;
+	}
+
+	template<CVertex TVertex>
+	spl::Buffer& Mesh<TVertex>::getElementBuffer()
+	{
+		return _ebo;
+	}
+
+	template<CVertex TVertex>
 	bool Mesh<TVertex>::isValid() const
 	{
 		return _vbo.isValid() && _ebo.isValid();
@@ -127,7 +209,11 @@ namespace lys
 	template<CVertex TVertex>
 	void Mesh<TVertex>::draw(const spl::ShaderProgram& program, const scp::f32mat4x4& transform) const
 	{
-		// TODO: assert on TVertex !
+		if constexpr (!std::same_as<TVertex, DefaultVertex>)
+		{
+			assert(false);
+		}
+
 		program.setUniform("model", transform * getTransformMatrix());
 		draw();
 	}
